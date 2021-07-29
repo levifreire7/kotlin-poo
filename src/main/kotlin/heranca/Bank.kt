57e369ce4b496eccd7ff6556a75b214b227ee9bf
levifreire7 <levifreire7@gmail.com>
2021-07-28 21:26:01 -0300

    Implementadas classes abstratas

---
+++ src/main/kotlin/heranca/Bank.kt
@@ -0,0 +1,27 @@
+package heranca
+
+abstract class Bank {
+    abstract fun simpleInterest(p: Int, r: Double, t: Int): Double
+}
+
+class SBI : Bank() {
+    override fun simpleInterest(p: Int, r: Double, t: Int): Double {
+        return (p * r * t) / 100
+    }
+}
+
+class PNB : Bank() {
+    override fun simpleInterest(p: Int, r: Double, t: Int): Double {
+        return (p * r * t) / 100
+    }
+}
+
+fun main() {
+    var sbi = SBI()
+    val sbiint = sbi.simpleInterest(1000, 5.0, 3)
+    println("SBI interest is $sbiint")
+
+    var pnb = PNB()
+    val pnbint = pnb.simpleInterest(1000, 4.5, 3)
+    println("PNB interest is $pnbint")
+}
